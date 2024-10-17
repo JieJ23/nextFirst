cd4af62bfbb363a2af43790662c20331c20f9c49
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-10-17 14:01:45 -0400

    Added current dev

--- app/about/ProcessDate.js
+++ app/about/ProcessDate.js
@@ -1,13 +1,14 @@
 "use client";
+import { Card, Typography, Button } from "@material-tailwind/react";
+import Link from "next/link";
 
-import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
-    Button,
-} from "@material-tailwind/react";
+const TABLE_HEAD = [
+    "Rank",
+    "Player",
+    "Tier",
+    "Build",
+    "Payment Date",
+];
 
 
 export default function ProcessData({ data }) {
@@ -15,35 +16,80 @@ export default function ProcessData({ data }) {
     const topPlayer = data.filter(obj => obj.Tier === 150)
     return (
         <div className="text-white">
-            <a href="/">Home</a>
-            <h2 className="text-4xl font-serif text-[orange] text-center">Player Data</h2>
-            <Card className="mt-6 w-96">
-                <CardHeader color="blue-gray" className="relative h-56" floated={false}>
-                    <img
-                        src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-                        alt="card-image"
-                    />
-                </CardHeader>
-                <CardBody>
-                    <Typography variant="h5" color="blue-gray" className="mb-2">
-                        UI/UX Review Check
-                    </Typography>
-                    <Typography>
-                        The place is close to Barceloneta Beach and bus stop just 2 min by
-                        walk and near to &quot;Naviglio&quot; where you can enjoy the main
-                        night life in Barcelona.
-                    </Typography>
-                </CardBody>
-                <CardFooter className="pt-0">
-                    <Button>Read More</Button>
-                </CardFooter>
-            </Card>
-            <ul>
-                {topPlayer.map((post, index) => (
-                    <li key={index}>{post.Player}</li>
-                ))}
-            </ul>
-            <div>{data.length}</div>
+            <div className="text-center my-10">
+                <Link href="/">
+                    <Button>Home</Button>
+                </Link>
+            </div>
+            <div className="px-2 mt-5 mb-16">
+                <Card className="h-full w-full max-w-[1400px] mx-auto backdrop-blur-md bg-black/50 border border-white/20 overflow-auto" shadow={false}>
+                    <table className="w-full min-w-max table-auto text-left">
+                        <thead>
+                            <tr>
+                                {TABLE_HEAD.map((head, index) => (
+                                    <th key={index} className="p-4 pt-10">
+                                        <Typography
+                                            variant="small"
+                                            className="font-bold leading-none text-gray-300"
+                                        >
+                                            {head}
+                                        </Typography>
+                                    </th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {topPlayer.map((obj, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td className="p-4">
+                                            <Typography
+                                                variant="small"
+                                                color="white"
+                                                className="font-bold"
+                                            >
+                                                {index + 1}
+                                            </Typography>
+                                        </td>
+                                        <td className="p-4">
+                                            <Typography
+                                                variant="small"
+                                                className="font-normal text-white"
+                                            >
+                                                {obj.Player}
+                                            </Typography>
+                                        </td>
+                                        <td className="p-4">
+                                            <Typography
+                                                variant="small"
+                                                className="font-normal text-white"
+                                            >
+                                                {obj.Tier}
+                                            </Typography>
+                                        </td>
+                                        <td className="p-4">
+                                            <Typography
+                                                variant="small"
+                                                className="font-normal text-white"
+                                            >
+                                                {obj["Build Name"]}
+                                            </Typography>
+                                        </td>
+                                        <td className="p-4">
+                                            <Typography
+                                                variant="small"
+                                                className="font-normal text-white"
+                                            >
+                                                {obj["Time Used"]}
+                                            </Typography>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </Card>
+            </div>
         </div>
     );
 }
