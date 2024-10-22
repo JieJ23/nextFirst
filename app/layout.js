3bebf1cc7f63048ac76feb4f6494b756635b8a93
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-10-22 17:45:21 -0400

    Finalized

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <link rel="icon" href="/icon.ico" sizes="any" />
-      <body className="bg-gradient-to-br from-[#2c2626] to-[#04040a] bg-center bg-fixed min-h-lvh"
+      <body className="bg-gradient-to-br from-[#2c2626] to-[#04040a] h-lvh bg-black"
       >
         {children}
       </body>
