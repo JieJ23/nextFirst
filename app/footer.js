5eed2b49c33436f60404cc561b6ecf0581cc934d
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-10-23 12:09:22 -0400

    Testing Server

--- app/footer.js
+++ app/footer.js
@@ -3,11 +3,14 @@
 export function Footer() {
     return (
         <div className="mt-10 py-5 flex flex-col gap-1 select-none">
-            <div className="text-gray-200 font-serif text-center text-[12px] px-2">
-                This website has no affiliation with or endorsement from Activision
-                Blizzard, Inc.
+            <div className="text-gray-400 font-serif text-center text-[12px] px-2">
+                This website has no affiliation with or endorsement from Supergaint
+                Games, LLC.
             </div>
-            <div className="text-gray-200 font-serif text-center text-[12px]">
+            <div className="text-gray-300 font-serif text-center text-[12px]">
+                Collaboration 小Joyboy2k
+            </div>
+            <div className="text-gray-400 font-serif text-center text-[12px]">
                 © 2024 CY. All rights reserved.
             </div>
         </div>
