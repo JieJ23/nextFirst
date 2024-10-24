99928bd8994cb6538d4a4c6ba97cbe47684736c6
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-10-24 11:25:29 -0400

    Testing no-store

--- app/contact/FetchData.js
+++ app/contact/FetchData.js
@@ -63,7 +63,7 @@ export default async function FetchData() {
         };
     })
 
-    const testing = await fetch(`http://worldtimeapi.org/api/timezone/America/New_York`)
+    const testing = await fetch(`http://worldtimeapi.org/api/timezone/America/New_York`, { cache: 'no-store' })
     const testingR = await testing.json()
     const results = testingR.datetime
 
